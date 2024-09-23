2bd97a7acf32ba7654798a2b5042402cafa58c92
aziz <aziz@DESKTOP-K184F5B>
2024-09-23 14:13:03 +0100

    theme

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace ToDoList
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-Lumen.css",
                       "~/Content/site.css"));
         }
     }
